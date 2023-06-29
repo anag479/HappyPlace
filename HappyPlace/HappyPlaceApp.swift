889eb02dc4c2c700f0068299adfd41efb6d6ff5c
Anjali Nag <naganjali06@gmail.com>
2023-06-29 12:45:26 -0500

    Initial Commit

---
+++ HappyPlace/HappyPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HappyPlaceApp.swift
+//  HappyPlace
+//
+//  Created by Scholar on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct HappyPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
